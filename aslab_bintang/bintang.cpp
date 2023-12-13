88f9276c830ff221bc22093959b214eb450b5cee
radinals <radinalsaragih@gmail.com>
2023-12-13 17:21:29 +0700

    added latihan

---
+++ aslab_bintang/bintang.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int size = 4;
+	for (int y = 0; y < size; y++) {
+
+
+		for (int x = 0; x < size*2 - y; x++)
+			cout << " ";
+
+		for (int x = 0; x <= y * 2; x++)
+			cout << "*";
+
+		for (int x = 0; x < size - y; x++)
+			cout << " ";
+
+		cout << endl;
+	}
+
+	for(int y = size*2; y > (size); y--)
+	{
+
+		for (int x = 0; x < size*2 - y; x++)
+			cout << " ";
+
+		for (int x = 0; x <= y * 2; x++)
+			cout << "*";
+		cout << endl;
+	}
+
+	for (int y = 1; y < size; y++) {
+
+		for (int x = 0; x < size - y; x++)
+			cout << " ";
+
+		for (int x = 0; x <= size - y; x++)
+			cout << "*";
+
+		for (int x = 0; x < y*4; x++)
+			cout << " ";
+
+		for (int x = 0; x <= size - y; x++)
+			cout << "*";
+
+		cout << endl;
+	}
+
+
+
+	return 0;
+}
---
+++ aslab_lingkaran/lingkaran.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int diameter = 32;
+	for(int y = 0; y <= diameter+2;y+=2)
+	{
+		if (y < diameter - diameter/2 ) continue;
+
+		int tmp = y; if (tmp > diameter) tmp = diameter;
+
+		for (int x=0; x < diameter*2-tmp; x++)
+			cout << " ";
+
+		for (int x=0; x < tmp; x++)
+		{
+			cout << "#" << " ";
+
+		}
+		cout << endl;
+	}
+
+	for(int y = diameter; y >= 0;y-=2)
+	{
+		if (y > diameter)
+			y = diameter;
+
+		if (y < diameter - diameter/2 ) continue;
+
+		for (int x=0; x < diameter*2-y; x++)
+			cout << " ";
+
+		for (int x=0; x < y; x++)
+		{
+			cout << "#" << " ";
+
+		}
+		cout << endl;
+	}
+	return 0;
+}
