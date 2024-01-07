65fbbf37c80ea54508481cf579d02109be6c0dce
radinals <radinalsaragih@gmail.com>
2024-01-07 13:21:49 +0700

    added error checking to the setters and getters, removed TODO

--- Alpemdas/alpemdas_dendapustaka/dendapustaka.cpp
+++ Alpemdas/alpemdas_dendapustaka/dendapustaka.cpp
@@ -2,6 +2,7 @@
 #include <ios>
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 
 using namespace std;
@@ -15,8 +16,8 @@ long long hitung_denda(string hari_dipinjam);
 template <typename T_NO, typename T_NAMA, typename T_HARI, typename T_DENDA>
 void print_data(T_NO no, T_NAMA nama, T_HARI hari, T_DENDA denda);
 
-void print_tabel(string data_peminjam[][2], int jumlah_peminjam);
-void input_tabel(string data_peminjam[][2], int jumlah_peminjam);
+void print_tabel(string data_peminjam[][2]);
+void input_tabel(string data_peminjam[][2]);
 
 string get_nama(string data[][2], size_t i);
 string get_hari(string data[][2], size_t i);
@@ -26,12 +27,17 @@ void set_hari(string data[][2], size_t i);
 const int besar_denda = 5000; // rupiah
 const int tempo_denda = 7;    // hari
 
+// jumlah data anggota yg akan disimpan
+int jumlah_peminjam = 0;
+
+// quick lamda for checking out of bound indexes
+// clang-format off
+auto validate_index = [](size_t index) -> void { if (index >= jumlah_peminjam) throw std::out_of_range("index out of bounds"); };
+// clang-format on
+
 int
 main()
 {
-	// jumlah data anggota yg akan disimpan
-	int jumlah_peminjam = 0;
-
 	cout << "Masukan Jumlah Data Peminjam: ";
 	cin >> jumlah_peminjam;
 
@@ -45,10 +51,10 @@ main()
 	string data_peminjam[jumlah_peminjam][2];
 
 	// inputkan data
-	input_tabel(data_peminjam, jumlah_peminjam);
+	input_tabel(data_peminjam);
 
 	// outputkan header dan outputkan data
-	print_tabel(data_peminjam, jumlah_peminjam);
+	print_tabel(data_peminjam);
 
 	cin.get();
 
@@ -56,23 +62,30 @@ main()
 }
 
 void
-input_tabel(string data_peminjam[][2], int jumlah_peminjam)
+input_tabel(string data_peminjam[][2])
 {
 	for (size_t i = 0; i < jumlah_peminjam; i++) {
 		string nama, hari;
 
 		cout << "\nNO: " << i + 1 << "\n";
 
-		cout << "Masukan Nama          : ";
-		set_nama(data_peminjam, i);
+		try {
+			cout << "Masukan Nama          : ";
+			set_nama(data_peminjam, i);
 
-		cout << "Masukan Hari Dipinjam : ";
-		set_hari(data_peminjam, i);
+			cout << "Masukan Hari Dipinjam : ";
+			set_hari(data_peminjam, i);
+
+		} catch (std::out_of_range) { // should never go off
+			cout << "ERROR: tried to set data with an invalid "
+			        "index!\n";
+			exit(-1);
+		};
 	}
 }
 
 void
-print_tabel(string data_peminjam[][2], int jumlah_peminjam)
+print_tabel(string data_peminjam[][2])
 {
 	// using lambda since it's relevant and used only in here
 	// FIXME: the len should be determine by the longest line in the
@@ -86,8 +99,15 @@ print_tabel(string data_peminjam[][2], int jumlah_peminjam)
 	for (size_t i = 0; i < jumlah_peminjam; i++) {
 		print_line();
 
-		string hari = get_hari(data_peminjam, i),
-		       nama = get_nama(data_peminjam, i);
+		string hari, nama;
+		try {
+			hari = get_hari(data_peminjam, i);
+			nama = get_nama(data_peminjam, i);
+		} catch (std::out_of_range) { // should never go off
+			cout << "ERROR: tried access data with an invalid "
+			        "index!\n";
+			exit(-1);
+		}
 
 		long long besar_denda = hitung_denda(hari);
 
@@ -133,28 +153,30 @@ hitung_denda(string hari_dipinjam)
 	return besar_denda * (str2num(hari_dipinjam) / tempo_denda);
 }
 
-// TODO: to be more useful, these getter and setters should
-//	 error check first before "getting" or "setting" a value
 string
 get_nama(string data[][2], size_t i)
 {
+	validate_index(i);
 	return data[i][0];
 }
 
 string
 get_hari(string data[][2], size_t i)
 {
+	validate_index(i);
 	return data[i][1];
 }
 
 void
 set_nama(string data[][2], size_t i)
 {
+	validate_index(i);
 	cin >> data[i][0];
 }
 
 void
 set_hari(string data[][2], size_t i)
 {
+	validate_index(i);
 	cin >> data[i][1];
 }
